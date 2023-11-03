3105db274b1fe1e3b8432af0e6a2cf56e8752493
ayunku <emmanueloyindeyefa@gmail.com>
2023-11-03 20:18:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,261 @@
+// console.log("hello world")
+// let day = "friday"
+// console.log(day)
+// const job = "developer"
+// console.log(job)
+// var network = "mtn"
+// console.log(network)
+// console.log(12)
+// let age = 92
+// console.log(age)
+// let title = " denyefa"
+// console.log(title)
+
+
+
+
+// let work = prompt("a full stack developer")
+// alert(work)
+
+
+// let rice = 0
+// let beans = 100
+// let garri = 30
+// let sugar = 100
+// const food = (rice*beans-garri/sugar)
+// console.log("the answer is " + food)
+
+
+
+
+// let firstNumber = prompt("input first number")
+// alert(firstNumber)
+// let secondNumber = prompt("input second Number")
+// alert(secondNumber)
+// let manipula_Tion = prompt("what manipulation do you wish to perform e.g + - * / = ")
+// alert(manipula_Tion)
+
+// switch(manipula_Tion) {
+//     case "*":
+//         console.log(firstNumber*secondNumber)
+//         break;
+//     case "-":
+//         console.log(firstNumber-secondNumber)
+//         break;
+//     case "+":
+//         console.log( Number(firstNumber)+Number(secondNumber))
+//         break;
+//     case "/":
+//         console.log(firstNumber/secondNumber)
+//         break;
+// }
+
+
+
+
+
+// function myfirstfunc () {
+//     console.log("wagwan jamie")
+// }
+// myfirstfunc()
+
+
+// let Topboy = ()=>{
+//     console.log("sully has to go innit")
+// }
+// Topboy()
+
+// let chemical = (value, vaid )=>{
+//     console.log("me")
+//     console.log("you")
+// }
+// chemical()
+
+
+// let callingback = (call) =>{
+//     console.log("i am the bad guy")
+//     call()
+// }
+// let callbacked = () =>{
+//     console.log("karma is not real")
+// }
+// callbacked(callingback)
+// callingback(callbacked)
+
+
+ let First_Number =prompt("kindly input first numbeer")
+ let Second_Number = prompt("kindly input second number")
+ let Third_Number = prompt("kindly input the third number")
+ let ManiPulation = prompt("which of these manipulation do you wish to perform eg. -,+,/ or *")
+ let value = ManiPulation
+
+
+  // const calCulate = (value1, value2, value3)=>{
+  //     const calculatenomba = value1 + value2 + value3
+  //     console.log(calculatenomba)
+  // }
+  // const manCulate = (value1, value2, value3)=>{ 
+  //   const subtract =value1 - value2 - value3
+  //   console.log(subtract)
+  // }
+  // const divCulate = (value1,value2,value3)=>{ 
+  //   const divide = value1 / value2 / value3
+  //   console.log(divide)
+  // }
+
+  //  if (ManiPulation === "+"){
+  //      calCulate(First_Number,Second_Number,Third_Number)
+  //  }
+  //  if (ManiPulation === "-"){
+  //    manCulate(First_Number,Second_Number,Third_Number)
+  //  }
+  //  if (ManiPulation === "/"){
+  //   divCulate(First_Number,Second_Number,Third_Number)
+  //  }
+
+
+
+
+
+
+
+
+
+
+   const calCulate = (value1, value2, value3)=>{
+    const calculatenomba = value1 + value2 + value3
+    const dividenomba = value1 / value2 / value3
+    const multiplynomba = value1 * value2 * value3
+    console.log(dividenomba)
+    console.log(calculatenomba)
+    console.log(multiplynomba)
+   }
+
+   if (First_Number && Second_Number && Third_Number && ManiPulation) {
+    // console.log(value)
+    if (ManiPulation === "+")
+    console.log(First_Number + Second_Number + Third_Number)
+    if (ManiPulation === "-")
+    console.log(First_Number - Second_Number - Third_Number)
+    if (ManiPulation === "/")
+    console.log(First_Number / Second_Number / Third_Number)
+   }
+
+
+
+   
+   
+   else{
+    console.log("kindly input the right info")
+  }
+
+
+
+// if (ManiPulation === "+"){
+//   calCulate(First_Number,Second_Number,Third_Number)
+// }
+// if (ManiPulation === "/"){
+//   dividenomba(value1, value2, value3)
+// }
+// if (ManiPulation === "*"){
+//   multiplynomba(value1, value2, value3)
+// }
+
+
+
+
+
+
+
+
+
+
+//  return statement
+
+//  const Calculator =(a,b,c)=>{
+//      const result = a + b + c
+//      return result
+//  }
+
+//  let theReturnedValue = Calculator(1,3,7)
+
+//   console.log(theReturnedValue)
+
+
+
+
+
+
+
+
+
+
+
+// IF ELSE STATEMENT
+// conditions
+
+// const check =  2
+
+// const isDone = 4
+
+// if (check){
+//     console.log(check,"Checking is true")
+// }else if(isDone){
+//     console.log(isDone,"The work is done")
+// }else{
+//     console.log(check,"Checking is false")
+// }
+
+// operators
+
+// > "Greater than"
+// < "Less than"
+// <= "Less than or equal"
+// >= "Greater thsn or equal"
+// == "Equals"
+// === "Equals"
+// ! "Negation"
+// != "Not equal"
+// !== "Not equal"
+// && "And"
+// || "or"
+
+// const sayYes = 0
+// const myAge = 100
+
+// const faithAge = 150
+
+// if (myAge > faithAge){
+//     console.log("Tony is older")
+// }else if (myAge === faithAge){
+//     console.log("They are same age")
+// }else if (!sayYes){ //Only when value is false
+//     console.log("Say yes is false")
+// }else if(myAge < faithAge){
+//     console.log("Faith is older")
+// }else{
+//     console.log("We don't know")
+// }
+
+// // switch statements
+// const Name = "Dehjjhihnyefa"
+
+// switch(Name){
+//     case "Tony":
+//         console.log("This is Tony")
+//         break;
+//     case "Faith":
+//         console.log("This is Faith")
+//         break
+//     case "Denyefa":
+//         console.log("This is Denyefa")
+//         break
+//     default:
+//         console.log("None")
+//         break
+// }
+
+
+// collect the first Number
+// collect the second Number
+// sign what manipiulation do yu want eg + - / *
